f6406de9dd3821c4e2ada5a6d935cbb8f8d5c032
Lasse <lasse@bomh.net>
2022-12-07 12:04:01 +0100

    Init build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  },
   plugins: [
     svelte(),
     WindiCSS(),
